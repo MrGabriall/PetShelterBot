b2253d53cde3aa40013985dd7f910a5f6062b952
MrGabriall <mrgabriall@gmail.com>
2022-12-21 21:13:32 +0300

    Initial commit

---
+++ src/main/java/com/skypro/java/petshelterbot/PetShelterBotApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.java.petshelterbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetShelterBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetShelterBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skypro/java/petshelterbot/PetShelterBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.java.petshelterbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetShelterBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
