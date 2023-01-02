502eabaeea65528b99b20c028031f3f7b9015b31
KiriukhinD <dm.arz2018@yandex.ru>
2023-01-02 21:27:38 +0300

    botOutMessages 1.0.2

--- src/main/java/com/skypro/java/petshelterbot/message/botOutMessages.java
+++ src/main/java/com/skypro/java/petshelterbot/message/botOutMessages.java
@@ -2,38 +2,38 @@ package com.skypro.java.petshelterbot.message;
 
 public class botOutMessages {
     //Консультация с новым пользователем
-    private static final String message1 = "Бот приветствует пользователя. ";
-    private static final String message2 = "Бот может рассказать о приюте.  ";
-    private static final String message3 = "Бот может выдать расписание работы приюта и адрес, схему проезда. ";
-    private static final String message4 = " Бот может выдать общие рекомендации о технике безопасности на   территории приюта. ";
-    private static final String message5 = "Бот может принять и записать контактные данные для связи. ";
-    private static final String message6 = " Если бот не может ответить на вопросы клиента, то можно позвать волонтера.";
+    private static final String newUserMessage1 = "Бот приветствует пользователя. ";
+    private static final String newUserMessage2 = "Бот может рассказать о приюте.  ";
+    private static final String newUserMessage3 = "Бот может выдать расписание работы приюта и адрес, схему проезда. ";
+    private static final String newUserMessage4 = " Бот может выдать общие рекомендации о технике безопасности на   территории приюта. ";
+    private static final String newUserMessage5 = "Бот может принять и записать контактные данные для связи. ";
+    private static final String newUserMessage6 = " Если бот не может ответить на вопросы клиента, то можно позвать волонтера.";
 
     //Консультация с потенциальным хозяином животного из приюта
-    private static final String message7 = "Бот приветствует пользователя.  ";
-    private static final String message8 = "Бот может выдать правила знакомства с собакой до того, как можно забрать ее из приюта. ";
-    private static final String message9 = "Бот может выдать список документов, необходимых для того, чтобы взять собаку из приюта. ";
-    private static final String message10 = "Бот может выдать список рекомендаций по транспортировке животного. ";
-    private static final String message11 = "Бот может выдать список рекомендаций по обустройству дома для щенка. ";
-    private static final String message12 = "Бот может выдать список рекомендаций по обустройству дома для взрослой собаки.  ";
-    private static final String message13 = "Бот может выдать список рекомендаций по обустройству дома для собаки с ограниченными возможностями (зрение, передвижение).  ";
-    private static final String message14 = "Бот может выдать советы кинолога по первичному общению с собакой. ";
-    private static final String message15 = "Бот может выдать рекомендации по проверенным кинологам для дальнейшего обращения к ним. ";
-    private static final String message16 = " Бот может выдать список причин, почему могут отказать и не дать забрать собаку из приюта. ";
-    private static final String message17 = " Бот может принять и записать контактные данные для связи.";
-    private static final String message18 = "Если бот не может ответить на вопросы клиента, то можно позвать волонтера. ";
+    private static final String potentialAnimalOwnerMessage7 = "Бот приветствует пользователя.  ";
+    private static final String potentialAnimalOwnerMessage8 = "Бот может выдать правила знакомства с собакой до того, как можно забрать ее из приюта. ";
+    private static final String potentialAnimalOwnerMessage9 = "Бот может выдать список документов, необходимых для того, чтобы взять собаку из приюта. ";
+    private static final String potentialAnimalOwnerMessage10 = "Бот может выдать список рекомендаций по транспортировке животного. ";
+    private static final String potentialAnimalOwnerMessage11 = "Бот может выдать список рекомендаций по обустройству дома для щенка. ";
+    private static final String potentialAnimalOwnerMessage12 = "Бот может выдать список рекомендаций по обустройству дома для взрослой собаки.  ";
+    private static final String potentialAnimalOwnerMessage13 = "Бот может выдать список рекомендаций по обустройству дома для собаки с ограниченными возможностями (зрение, передвижение).  ";
+    private static final String potentialAnimalOwnerMessage14 = "Бот может выдать советы кинолога по первичному общению с собакой. ";
+    private static final String potentialAnimalOwnerMessage15 = "Бот может выдать рекомендации по проверенным кинологам для дальнейшего обращения к ним. ";
+    private static final String potentialAnimalOwnerMessage16 = " Бот может выдать список причин, почему могут отказать и не дать забрать собаку из приюта. ";
+    private static final String potentialAnimalOwnerMessage17 = " Бот может принять и записать контактные данные для связи.";
+    private static final String potentialAnimalOwnerMessage18 = "Если бот не может ответить на вопросы клиента, то можно позвать волонтера. ";
 
     //Ведение питомца
-    private static final String message19 = " Бот может прислать форму ежедневного отчета. ";
-    private static final String message20 = "Если пользователь прислал только фото, то бот может запросить текст.  ";
-    private static final String message21 = "Если пользователь прислал только текст, то бот может запросить фото.  ";
-    private static final String message22 = " Бот может выдать предупреждение о том, что отчет заполняется плохо (делает волонтер): \n" + "  " +
+    private static final String petManagementMessage19 = " Бот может прислать форму ежедневного отчета. ";
+    private static final String petManagementMessage20 = "Если пользователь прислал только фото, то бот может запросить текст.  ";
+    private static final String petManagementMessage21 = "Если пользователь прислал только текст, то бот может запросить фото.  ";
+    private static final String petManagementMessage22 = " Бот может выдать предупреждение о том, что отчет заполняется плохо (делает волонтер): \n" + "  " +
             "Дорогой усыновитель, мы заметили, что ты заполняешь отчет не так подробно, как необходимо. Пожалуйста, подойди ответственнее " +
             "к  этому занятию. В противном случае волонтеры приюта будут обязаны самолично проверять условия содержания собаки";
-    private static final String message23 = "Если усыновитель прошел испытательный срок, то бот поздравляет его стандартным сообщением.  ";
-    private static final String message24 = " Если усыновителю было назначено дополнительное время испытательного срока, то бот сообщает ему и указывает количество дополнительных дней. ";
-    private static final String message25 = " Если усыновитель не прошел испытательный срок, то бот уведомляет его об этом и дает инструкции по дальнейшим шагам. ";
-    private static final String message26 = "Если бот не может ответить на вопросы клиента, то можно позвать волонтера.  ";
+    private static final String petManagementMessage23 = "Если усыновитель прошел испытательный срок, то бот поздравляет его стандартным сообщением.  ";
+    private static final String petManagementMessage24 = " Если усыновителю было назначено дополнительное время испытательного срока, то бот сообщает ему и указывает количество дополнительных дней. ";
+    private static final String petManagementMessage25 = " Если усыновитель не прошел испытательный срок, то бот уведомляет его об этом и дает инструкции по дальнейшим шагам. ";
+    private static final String petManagementMessage26 = "Если бот не может ответить на вопросы клиента, то можно позвать волонтера.  ";
 
 
 }
