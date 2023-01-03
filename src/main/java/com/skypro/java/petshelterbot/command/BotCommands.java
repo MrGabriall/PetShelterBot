adf9cc42d40e007aaf41caf99c93a786607300da
evnag <vskonev22@gmail.com>
2023-01-03 19:28:06 +0300

    Add class w/ bot commands constants

---
+++ src/main/java/com/skypro/java/petshelterbot/command/BotCommands.java
@@ -0,0 +1,17 @@
+package com.skypro.java.petshelterbot.command;
+
+/*
+The class describes the commands that the bot can process
+ */
+
+public class BotCommands {
+
+    public static final String START = "/start";
+
+    public static final String INFO = "Узнать информацию о приюте";
+    public static final String HOW_TO_ADOPT = "Как взять животное из приюта";
+    public static final String CALL_VOLUNTEER = "Позвать волонтера";
+    public static final String SEND_REPORT = "Отправить отчет о питомце";
+
+    public static final String UNKNOWN_COMMAND = "Неизвестная комманда";
+}
