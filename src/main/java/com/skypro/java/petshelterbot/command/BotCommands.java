eb69416d403a25435d721a033c9130f122ed8e9b
evnag <vskonev22@gmail.com>
2023-01-07 18:47:24 +0300

    feat: add some command constants

--- src/main/java/com/skypro/java/petshelterbot/command/BotCommands.java
+++ src/main/java/com/skypro/java/petshelterbot/command/BotCommands.java
@@ -7,11 +7,28 @@ The class describes the commands that the bot can process
 public class BotCommands {
 
     public static final String START = "/start";
+    public static final String SELECT_OPTION = "Выберите пункт меню";
+    public static final String UNKNOWN_COMMAND = "Неизвестная комманда";
 
-    public static final String INFO = "Узнать информацию о приюте";
-    public static final String HOW_TO_ADOPT = "Как взять животное из приюта";
     public static final String CALL_VOLUNTEER = "Позвать волонтера";
     public static final String SEND_REPORT = "Отправить отчет о питомце";
 
-    public static final String UNKNOWN_COMMAND = "Неизвестная комманда";
+    public static final String INFO = "Информация о приюте";
+    public static final String HOW_TO_ADOPT = "Как взять животное из приюта";
+
+    public static final String TO_MAIN_MENU = "В главное меню";
+    public static final String ABOUT_SHELTER = "О Приюте";
+    public static final String SHELTER_ADDRESS = "Адрес приюта";
+    public static final String SAFETY_MEASURES = "О технике безопасности";
+    public static final String SEND_CONTACTS = "Передать контактные данные";
+
+    public static final String RULES_BEFORE_ADOPTING = "Правила знакомства с собакой";
+    public static final String REQUIRED_DOCUMENTS = "Список необходимых документов";
+    public static final String PET_TRANSPORTATION = "Рекомендаций по транспортировке животного";
+    public static final String PUPPY_HOUSE = "Как обустроить дом для щенка";
+    public static final String ADULT_DOG_HOUSE = "Как обустроить дом для взрослой собаки";
+    public static final String DOG_HANDLERS_TIPS = "Советы кинолога";
+    public static final String DOG_HANDLERS_LIST = "Список профессиональных кинологов";
+    public static final String WHY_REJECTED = "Почему мне отказали?";
+
 }
