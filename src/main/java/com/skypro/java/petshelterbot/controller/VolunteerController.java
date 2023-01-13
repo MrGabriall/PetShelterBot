4329725ad66f385d503cde6298e24c218d60a2c4
KiriukhinD <dm.arz2018@yandex.ru>
2023-01-13 11:25:09 +0300

    Volunteer 1.0.0.1

--- src/main/java/com/skypro/java/petshelterbot/controller/VolunteerController.java
+++ src/main/java/com/skypro/java/petshelterbot/controller/VolunteerController.java
@@ -2,6 +2,11 @@ package com.skypro.java.petshelterbot.controller;
 
 import com.skypro.java.petshelterbot.entity.Volunteer;
 import com.skypro.java.petshelterbot.service.VolunteerService;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,16 +22,50 @@ public class VolunteerController {
         this.volunteerService = volunteerService;
     }
 
+
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "created volunteer",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = Volunteer[].class)
+                    )
+            )
+    })
+
+
     @PostMapping("/create")
     public Volunteer create(@RequestBody Volunteer volunteer) {
         return volunteerService.create(volunteer);
     }
 
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "read volunteer",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = Volunteer[].class)
+                    )
+            )
+    })
     @GetMapping("/read/{id}")
     public Volunteer read(@PathVariable long id) {
         return volunteerService.read(id);
 
     }
+
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "update volunteer",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = Volunteer[].class)
+                    )
+            )
+    })
 
     @PutMapping("/update/{id}")
     public ResponseEntity<Volunteer> update(@RequestBody Volunteer volunteer) {
@@ -38,11 +77,33 @@ public class VolunteerController {
 
     }
 
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "deleted volunteer",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = Volunteer[].class)
+                    )
+            )
+    })
+
     @DeleteMapping("/delete/{id}")
     public void delete(@PathVariable long id) {
         volunteerService.delete(id);
 
     }
+
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "All volunteeer",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = Volunteer[].class)
+                    )
+            )
+    })
 
     @GetMapping("/showVolunteer")
     public List showVolunteer() {
--- src/main/java/com/skypro/java/petshelterbot/service/VolunteerService.java
+++ src/main/java/com/skypro/java/petshelterbot/service/VolunteerService.java
@@ -9,37 +9,72 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+/**
+ * @author KiriukhinD
+ */
 @Service
 public class VolunteerService {
     private final VolunteerRepository volunteerRepository;
     Logger logger = LoggerFactory.getLogger(VolunteerService.class);
 
+    /**
+     * @param volunteerRepository
+     * @see Volunteer#Volunteer()
+     */
     VolunteerService(VolunteerRepository volunteerRepository) {
         this.volunteerRepository = volunteerRepository;
     }
 
-
+    /**
+     * method create volunteer
+     *
+     * @param volunteer
+     * @return new obj volunterr
+     */
     public Volunteer create(Volunteer volunteer) {
         logger.info("method [create]");
         return volunteerRepository.save(volunteer);
     }
 
+    /**
+     * method read volunteer
+     *
+     * @param id
+     * @return obj volunteer
+     */
     public Volunteer read(long id) {
         logger.info("method [read]");
         return volunteerRepository.findById(id).get();
     }
 
+    /**
+     * method update volunteer
+     *
+     * @param volunteer
+     * @return obj new volunteer
+     */
     public Volunteer update(Volunteer volunteer) {
         logger.info("method [update]");
         return volunteerRepository.save(volunteer);
     }
 
+    /**
+     * method delete volunteer
+     *
+     * @param id
+     * @return
+     */
     public ResponseEntity<Volunteer> delete(long id) {
         logger.info("method [delete]");
         volunteerRepository.deleteById(id);
         return ResponseEntity.ok().build();
     }
 
+    /**
+     * method of getting the field value
+     *
+     * @return all list Volunteer
+     */
     public List showVolunteer() {
         return volunteerRepository.showVolunteer();
     }
